a6c9bfe33b15281f8ceee71f313b47b486c85ee0
champa1n73 <giakhuong0703@gmail.com>
2023-12-06 23:49:54 +0700

    Updae Query

--- src/Query/CinemaBookingQuery.sql
+++ src/Query/CinemaBookingQuery.sql
@@ -8,18 +8,18 @@ go
 create table Users
 (
 	userName varchar(32) primary key,
-	userPassword varchar(32),
-	firstName varchar(20),
-	lastName varchar(20)
+	userPassword varchar(32) not null,
+	firstName varchar(20) not null,
+	lastName varchar(20) not null
 
 )
 
 create table BankAccount
 (
-	userName varchar(32),
+	userName varchar(32) not null,
 	bankNumber int primary key,
-	bankMoney money,
-	bankOTP int,
+	bankMoney money not null,
+	bankOTP int not null,
 
 	constraint FK_BankAccount_Users
 	foreign key (userName) references Users(userName)
@@ -28,24 +28,24 @@ create table BankAccount
 create table Movies
 (
 	movieID int identity(1, 1) primary key,
-	movieName varchar(50),
-	duration time,
-	genre varchar(20),
-	directorName varchar(50)
+	movieName varchar(50) not null,
+	duration time not null,
+	genre varchar(20) not null,
+	directorName varchar(50) not null
 )
 
 create table Cinemas
 (
 	cinemaID int identity(1, 1) primary key,
-	cinemaName varchar(50),
-	cinemaAddress varchar(100)
+	cinemaName varchar(50) not null,
+	cinemaAddress varchar(100) not null
 )
 
 create table Auditorium
 (
-	cinemaID int,
+	cinemaID int not null,
 	auditoriumID int identity(1, 1) primary key,
-	auditoriumName varchar(50),
+	auditoriumName varchar(50) not null,
 	
 	constraint FK_Auditorium_Cinemas
 	foreign key (cinemaID) references Cinemas(cinemaID)
@@ -53,10 +53,10 @@ create table Auditorium
 
 create table Seats
 (
-	auditoriumID int,
+	auditoriumID int not null,
 	seatID int identity(1, 1) primary key,
-	statusSeat varchar(20),
-	rowLocation varchar(10),
+	statusSeat varchar(20) not null,
+	rowLocation varchar(10) not null,
 
 	constraint FK_Seats_Auditorium
 	foreign key (auditoriumID) references Auditorium(auditoriumID)
@@ -65,11 +65,12 @@ create table Seats
 create table Schedule
 (
 	scheduleID int identity(1, 1) primary key,
-	movieID int,
-	auditoriumID int,
-	scheduleDate datetime,
-	totalTicket int,
-	ticketsSold int,
+	movieID int not null,
+	auditoriumID int not null,
+	scheduleDate datetime not null,
+	totalTicket int not null,
+	ticketsSold int not null,
+	ticketPrice money not null,
 
 	constraint FK_Schedule_Movies
 	foreign key (movieID) references Movies(movieID),
@@ -82,11 +83,11 @@ create table Schedule
 create table Reservation
 (
 	reserveID int identity(1, 1) primary key,
-	userName varchar(32),
-	scheduleID int,
-	seatID int,
-	reserveDate datetime,
-	paymentStatus varchar(20),
+	userName varchar(32) not null,
+	scheduleID int not null,
+	seatID int not null,
+	reserveDate datetime not null,
+	paymentStatus varchar(20) not null,
 
 	constraint FK_Reservation_Users 
 	foreign key (userName) references Users(userName),
@@ -101,10 +102,10 @@ create table Reservation
 create table Payment
 (
 	paymentID int identity(1, 1) primary key,
-	userName varchar(32),
-	reserveID int,
-	amount money,
-	paymentDate datetime,
+	userName varchar(32) not null,
+	reserveID int not null,
+	amount money not null,
+	paymentDate datetime not null,
 
 	constraint FK_Payment_Users
 	foreign key (userName) references Users(userName),
@@ -115,4 +116,3 @@ create table Payment
 
 
 
-	
